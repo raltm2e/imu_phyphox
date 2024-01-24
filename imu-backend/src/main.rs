5f28eded5a18367afe94f8be2bcf6105ee8f88cb
Robert Altmäe <robert.altmae@cr14.ee>
2024-01-24 16:55:03 +0200

    Add /health endpoint for server status check

--- imu-backend/src/main.rs
+++ imu-backend/src/main.rs
@@ -1,9 +1,9 @@
 use actix_web::{App, HttpServer};
-use imu_backend::routes::imudata::{hello, imudata_file};
+use imu_backend::routes::imudata::{health, imudata_file};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello).service(imudata_file))
+    HttpServer::new(|| App::new().service(health).service(imudata_file))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
